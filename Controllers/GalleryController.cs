84c1e940b6f24e72a5a650fd385f948760bea0a5
Denis Vishnevski <itsmoyapochta@gmail.com>
2022-10-10 21:31:58 +0400

    GalleryController refactoring

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -1,5 +1,6 @@
 ﻿using AndreyRudenko.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Enumeration;
 
 namespace AndreyRudenko.Controllers
 {
@@ -7,29 +8,40 @@ namespace AndreyRudenko.Controllers
     [Route("[controller]")]
     public class GalleryController : ControllerBase
     {
-        private string sourceDirectoryPortraits = @"ClientApp\public\galleries\portraits";
-        private string sourceDirectoryFamily = @"ClientApp\public\galleries\family";
-        private string sourceDirectoryEvents = @"ClientApp\public\galleries\events";
+        private IWebHostEnvironment environment;
+
+        public GalleryController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        private string sourceDirectoryPortraits = @"galleries\portraits";
+        private string sourceDirectoryFamily = @"galleries\family";
+        private string sourceDirectoryEvents = @"galleries\events";
 
         [HttpGet]
         public IActionResult Get()
         {
+            string correctPath = environment.WebRootPath;
+            if (correctPath == null)
+            {
+                correctPath = Path.Combine(environment.ContentRootPath, @"ClientApp\public");
+            }
             Galleries newGallery = new()
             {
-                Portraits = Ff(sourceDirectoryPortraits).ToArray(),
-                Family = Ff(sourceDirectoryFamily).ToArray(),
-                Events = Ff(sourceDirectoryEvents).ToArray()
+                Portraits = ImageList(Path.Combine(correctPath, sourceDirectoryPortraits)).ToArray(),
+                Family = ImageList(Path.Combine(correctPath, sourceDirectoryFamily)).ToArray(),
+                Events = ImageList(Path.Combine(correctPath, sourceDirectoryEvents)).ToArray()
             };
             return Ok(newGallery);
         }
-        private List<Image> Ff(string sourceDirectory)
+        private List<Image> ImageList(string sourceDirectory)
         {
             List<Image> sourceList = new List<Image>();
             string[] allFiles = Directory.GetFiles(sourceDirectory);
             int imageId = 1;
-            foreach (string filePath in allFiles)
+            foreach (var file in allFiles)
             {
-                string fileName = Path.GetFileName(filePath);
+                string fileName = Path.GetFileName(file);
                 Image image = new()
                 {
                     Id = imageId,
